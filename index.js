88f6785a943edb70f08b6bc5782c1f91a88378f3
SpiritOTHawk <karimdzhumakov21082007@gmail.com>
2023-03-22 22:47:16 +0300

    Logging and changes in createWindow()

--- index.js
+++ index.js
@@ -4,30 +4,45 @@ const url = require('url').format;
 
 let win
 const iconPath = path(__dirname, 'docs/icons/icon.png');
+console.log("Found icon: ", iconPath);
 
 async function createWindow() {
     win = new BrowserWindow({
         width: 400,
         height: 600,
+        fullscreenable: false,
+        resizable: false,
         icon: iconPath,
-        titleBarStyle: '',
         autoHideMenuBar: true,
-        titleBarOverlay: {
-            color: '#2F353B',
-            symbolColor: '#C7D0CC'
-        },
+        spellcheck: false,
+        experimentalFeatures: true,
         webPreferences: {
-            nodeIntegrationInWorker: true
+            nodeIntegration: true,
+            nodeIntegrationInSubFrames: true,
+            nodeIntegrationInWorker: true,
+            webgl: false,
+            enableWebSQL: false,
+            v8CacheOptions: "bypassHeatCheck"
         }
     });
 
     win.loadFile('./web/index.html');
+    console.log("App loaded");
+    getProcessInfo();
 
     win.on('closed', () => {
         win = null;
+        console.log("App closed");
     });
 };
 
+async function getProcessInfo() {
+    console.log("App version: ", app.getVersion());
+    console.log("Process started with PID: ", process.pid);
+    console.log(process.versions);
+};
+
+console.log("App ready! Loading application.");
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
